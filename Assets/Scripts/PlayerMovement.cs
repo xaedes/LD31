2ab9c58de972836f18ab635e7d23fb8a747d6c71
xaedes <xaedes@googlemail.com>
2014-12-06 11:50:37 +0100

    remastered animations and move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,20 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 	Animator anim;                      // Reference to the animator component.
-	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+	Vector3 movement;
+	public float speed = 4f;
 
 	// Use this for initialization
 	void Awake ()
 	{
 		// Set up references.
 		anim = GetComponent <Animator> ();
-		playerRigidbody = GetComponent <Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
-	
+
 	}
 
 	void FixedUpdate()
@@ -26,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 
+		movement.Set(0,0,0);
 		// Move the player around the scene.
 		Move (h, v);
 		
@@ -38,7 +39,10 @@ public class PlayerMovement : MonoBehaviour
 	void Move (float h, float v)
 	{
 		// Move the player to it's current position plus the movement.
-		playerRigidbody.AddForce(new Vector3(h,0f,v).normalized * 10,ForceMode.VelocityChange);
+
+		movement += (new Vector3(h,0f,v)).normalized * speed * Time.deltaTime;
+
+		transform.position += movement;
 	}
 	void Turning ()
 	{
