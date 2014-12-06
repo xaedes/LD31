d4eb9bec3b4aa11e25ec88ec55cc8187846ee3fa
xaedes <xaedes@googlemail.com>
2014-12-06 09:44:56 +0100

    load walls of level

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour
+{
+	Text text;
+	public Transform wall;
+	// Use this for initialization
+	void Start()
+	{
+		text = GetComponent <Text>();
+
+		string str = text.text;
+
+		var lines = str.Split('\n');
+		int height = lines.Length;
+
+		float blocksize = 4f;
+		for (int j = 0; j < height; j++) {
+			int width = lines [j].Length;
+			for (int i = 0; i < width; i++) {
+				switch (lines [j] [i]) {
+					case 'w':
+						Instantiate(wall, new Vector3(i * blocksize, 0, j * blocksize), Quaternion.identity);
+						break;
+					case 'A':
+						break;
+					case 'b':
+						break;
+					case 'B':
+						break;
+					case 'E':
+						break;
+					default:
+						break;
+				}	
+
+			}
+		}
+	}
+    
+	// Update is called once per frame
+	void Update()
+	{
+//  text.text = "Score: ";
+	}
+}
+
+
+
+
