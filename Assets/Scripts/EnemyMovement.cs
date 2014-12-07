b85e82e30840d1241bf17c0339e5d4a604ec4e16
xaedes <xaedes@googlemail.com>
2014-12-07 23:25:10 +0100

    improved enemy movement, increased speed, disabled

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,56 +4,65 @@ using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
 	public float speed;
 	NavMeshAgent nav;
 	Rigidbody rigid;
-	Animator anim;          
-
+	Animator anim;
 	Vector3 target;
 	Vector3 movement;
 	Grid grid;
 	HasGridValues values;
-	
 	const float movingThreshold = 1e-2f;
-	const float stopOnDistance = 1e-1f;
+	const float stopOnDistance = 0.1f;
 
 	// Use this for initialization
-	void Awake () {
+	void Awake()
+	{
 		nav = GetComponent <NavMeshAgent>();
 		anim = GetComponent <Animator>();
 		rigid = GetComponent <Rigidbody>();
 		target = transform.position;
-		grid = GameObject.FindGameObjectWithTag ("Grid").GetComponent<Grid>();
+		grid = GameObject.FindGameObjectWithTag("Grid").GetComponent<Grid>();
 		values = GetComponent<HasGridValues>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		Animating();
-		if(movement.magnitude < stopOnDistance) {
+		if (movement.magnitude < stopOnDistance) {
 			SelectTarget();
 		}
+
 	}
 
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate()
+	{
 		movement = target - transform.position;
 		movement.y = 0;
-		if(movement.magnitude > movingThreshold){
-			movement = movement.normalized * Time.deltaTime * speed;
+		if (movement.magnitude > movingThreshold) {
+			float length = Time.deltaTime * speed;
+
+			if( movement.magnitude < length ) {
+				length = movement.magnitude;
+			}
+			movement = movement.normalized * length;
 			nav.Move(movement);
 		} else {
 			movement = Vector3.zero;
 		}
 	}
 
-	void SelectTarget() {
+	void SelectTarget()
+	{
 		Vector3[] ns = grid.EmptyNeighbors4(Grid.round(transform.position));
 
 
-		if(ns.Length > 0) {
-			target = ns.MinBy(n => values["fart",Grid.index(n)]);
+		if (ns.Length > 0) {
+			target = ns.MinBy(n => values ["fart", Grid.index(n)]);
 //			target = ns[Random.Range(0,ns.Length-1)];
 		}
 	}
