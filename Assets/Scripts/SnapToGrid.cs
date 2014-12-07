74e534bb8b34deb498bb1fd8f6775fa0b29edb4b
xaedes <xaedes@googlemail.com>
2014-12-07 14:03:28 +0100

    use SnapToGrid to fix position of boxes

--- Assets/Scripts/SnapToGrid.cs
+++ Assets/Scripts/SnapToGrid.cs
@@ -5,26 +5,9 @@ public class SnapToGrid : MonoBehaviour
 {
 	public float smoothness = 0.01f;
 	
-	// Use this for initialization
-	void Start()
-	{
-	
-	}
-	
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		Vector3 tilePos = transform.position / Grid.blocksize;
-		Vector3 target = transform.position;
-
-		if (Mathf.Abs(tilePos.x - Mathf.RoundToInt(tilePos.x)) >
-			Mathf.Abs(tilePos.z - Mathf.RoundToInt(tilePos.z))) {
-			target =  new Vector3(transform.position.x, transform.position.y, Mathf.RoundToInt(tilePos.z)*Grid.blocksize);
-		} else 
-		if (Mathf.Abs(tilePos.x - Mathf.RoundToInt(tilePos.x)) <
-			Mathf.Abs(tilePos.z - Mathf.RoundToInt(tilePos.z))) {
-			target = new Vector3(Mathf.RoundToInt(tilePos.x)*Grid.blocksize, transform.position.y, transform.position.z);
-		}
-		transform.position = Vector3.Lerp(transform.position, target, smoothness * Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, Grid.round(transform.position), smoothness * Time.deltaTime);
 	}
 }
